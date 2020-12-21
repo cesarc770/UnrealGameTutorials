c6bedc592599fd65a5de784b411be6e8024221a5
cesar caceres <cesarc770@gmail.com>
2020-12-21 12:25:21 -0700

    added c++ implementation for gamestart and gameover

--- ToonTanks/Source/ToonTanks/GameModes/TankGameModeBase.h
+++ ToonTanks/Source/ToonTanks/GameModes/TankGameModeBase.h
@@ -6,9 +6,8 @@
 #include "GameFramework/GameModeBase.h"
 #include "TankGameModeBase.generated.h"
 
-/**
- * 
- */
+class APawnTurret;
+class APawnTank;
 UCLASS()
 class TOONTANKS_API ATankGameModeBase : public AGameModeBase
 {
@@ -16,8 +15,13 @@ class TOONTANKS_API ATankGameModeBase : public AGameModeBase
 
 
 private:
+	APawnTank* PlayerTank;
+	int32 TargetTurrets = 0;
+
 	void HandleGameStart();
 	void HandleGameOver(bool PlayerWon);
+
+	int32 GetTargetTurretCount();
 
 public:
 	void ActorDied(AActor* DeadActor);
--- ToonTanks/Source/ToonTanks/Pawns/PawnBase.h
+++ ToonTanks/Source/ToonTanks/Pawns/PawnBase.h
@@ -34,13 +34,13 @@ public:
 	// Sets default values for this pawn's properties
 	APawnBase();
 
+	virtual void HandleDestruction();
+
 protected:
 	
 	void RotateTurret(FVector LookAtTarget);
 
 	void Fire();
-
-	virtual void HandleDestruction();
 
 	
 
